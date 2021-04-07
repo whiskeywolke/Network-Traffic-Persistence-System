decf63b001c6f7241f374787b1e913086c1d08dd
Michael Mente <a01634435@unet.univie.ac.at>
2021-04-07 18:15:12 +0200

    add measurements & test boost library

--- implementation/pcappp-test/main.cpp
+++ implementation/pcappp-test/main.cpp
@@ -19,20 +19,16 @@
 
 //#include <PcapFileDevice.h>
 int main() {
-    std::cout << "\n\n\n\n\nHello, World!" << std::endl;
+    std::cout << "\n\n\n\n\n"<< std::endl;
 
     /// Reading the file/ creating the reader
-
     auto start2 = std::chrono::high_resolution_clock::now();
-
     pcpp::PcapFileReaderDevice reader("./testfiles/equinix-nyc.dirA.20180517-125910.UTC.anon.pcap");
     //pcpp::PcapFileReaderDevice reader("./testfiles/example.pcap");
 
     auto end2 = std::chrono::high_resolution_clock::now();
     auto duration2 = std::chrono::duration_cast<std::chrono::nanoseconds>(end2-start2).count();
-    std::cout<<"reading file duration: "<<duration2<<" nanoseconds\n";
-
-
+    std::cout<<"reading file duration: "<<duration2<<" nanoseconds\n\n";
     if (!reader.open())
     {
         printf("Error opening the pcap file\n");
@@ -40,26 +36,26 @@ int main() {
     }
 
 
+
     ///parsing the file
-
     pcpp::RawPacketVector rpv;
-
     auto start3 = std::chrono::high_resolution_clock::now();
-
-    reader.getNextPackets(rpv, 100);
-
+    reader.getNextPackets(rpv);
+    pcap_stat stat{};
+    reader.getStatistics(stat);
+    reader.close();
     auto end3 = std::chrono::high_resolution_clock::now();
     auto duration3 = std::chrono::duration_cast<std::chrono::nanoseconds>(end3-start3).count();
-    std::cout<<"parsing duration: "<<duration3<<" nanoseconds\n";
 
-
-
+    if(stat.ps_recv!=rpv.size()){
+        std::cout<<"packetcount does not match\n";
+    }
     std::cout<<"packet count: "<<rpv.size()<<std::endl;
+    std::cout<<"Parsing time per packet: "<<duration3/rpv.size()<<" \ttotaltime: "<< duration3<<std::endl;
 
-    long IPCounter = 0;
-    long IPv4Counter = 0;
-    long IPv6ounter = 0;
-
+    long unsigned IPCounter = 0;
+    long unsigned IPv4Counter = 0;
+    long unsigned IPv6ounter = 0;
     for(pcpp::Packet p : rpv){
         if(p.isPacketOfType(pcpp::IP)){
             ++IPCounter;
@@ -69,25 +65,27 @@ int main() {
         if (p.isPacketOfType(pcpp::IPv6))
             ++IPv6ounter;
     }
-    reader.close();
+    if(IPCounter != IPv4Counter+IPv6ounter){
+        std::cout<<"ip != ipv4+ipv6\n"<<
+                    "\nIP Counter "<< IPCounter<<
+                    "\nIPv4 Counter "<<IPv4Counter<<
+                    "\nIPv6 Counter "<<IPv6ounter<<std::endl;
+    }
+    if(IPCounter!= rpv.size()){
+        std::cout<<"some packets are not IP: ";
+        std::cout<<"total packet count: "<<rpv.size()<<std::endl;
 
-    std::cout<<"Parsing time per packet: "<<duration3/rpv.size();
-    std::cout<<"\nIP Counter "<< IPCounter<<
-             "\nIPv4 Counter "<<IPv4Counter<<
-             "\nIPv6 Counter "<<IPv6ounter;
-
-
-    ///reading single packets
-
+    }
     std::cout<<std::endl;
 
-    //pcpp::Packet p = rpv.at(43);
 
 
-
-    //for(pcpp::Packet p : rpv){
+    ///reading single packets & converting to IPTUPLE format
+    int skippedCount = 0;
     std::vector<IPTuple> tuples{};
-     for(size_t i = 0; i < rpv.size(); ++i){
+
+    auto start4 = std::chrono::high_resolution_clock::now();
+    for(size_t i = 0; i < rpv.size(); ++i){
          pcpp::Packet p = rpv.at(i);
         if(p.getFirstLayer()->getProtocol() == pcpp::Ethernet){
             p.removeFirstLayer();
@@ -99,134 +97,72 @@ int main() {
             continue;
         }
        if(p.isPacketOfType(pcpp::IPv4)){
-            IPTuple t  = IPTuple(p.getLayerOfType<pcpp::IPv4Layer>()->getSrcIpAddress(),
-                                 p.getLayerOfType<pcpp::IPv4Layer>()->getDstIpAddress(),
-                                 3,
-                                4);
-            tuples.emplace_back(t);
-        }
-        else if(p.isPacketOfType(pcpp::IPv6)){
-            tuples.emplace_back(p.getLayerOfType<pcpp::IPv6Layer>()->getSrcIpAddress(),
-                                     p.getLayerOfType<pcpp::IPv6Layer>()->getDstIpAddress(),
+                IPTuple t  = IPTuple(p.getLayerOfType<pcpp::IPv4Layer>()->getSrcIpAddress(),
+                                     p.getLayerOfType<pcpp::IPv4Layer>()->getDstIpAddress(),
                                      3,
-                                     4);
+                                    4);
+                tuples.emplace_back(t);
+            }
+       else if(p.isPacketOfType(pcpp::IPv6)){
+                tuples.emplace_back(p.getLayerOfType<pcpp::IPv6Layer>()->getSrcIpAddress(),
+                                         p.getLayerOfType<pcpp::IPv6Layer>()->getDstIpAddress(),
+                                         3,
+                                         4);
+            }
+       else{
+            ++skippedCount;
         }
-
-/*
-              try {
-                   pcpp::ProtocolType outer = p.getFirstLayer()->getProtocol();
-                   p.removeFirstLayer();
-                   pcpp::ProtocolType inner = p.getFirstLayer()->getProtocol();
-                   if (outer != pcpp::IPv4) {
-                       printf("outer is not IPV4: %li\n", outer);
-                   }
-                   if (inner != pcpp::TCP && inner != pcpp::UDP) {
-                       printf("inner is not tcp or udp: %li\n\n", inner);
-                   }
-               }catch(...) {  }
-               */
-               /* if(outer != pcpp::IPv4 || inner != pcpp::TCP|| inner != pcpp::UDP) //2 = ipv4, 8 = tcp, 10 = udp
-                    std::cout<<"first protocol: "<<std::hex <<outer<<"\nsecond protocol: "<<std::hex <<inner<<"\n*****************************\n\n";
-            */
     }
-    std::cout<<"tuples size: "<<tuples.size()<<std::endl;
-     /// serialization with boost
+    auto end4 = std::chrono::high_resolution_clock::now();
+    auto duration4 = std::chrono::duration_cast<std::chrono::nanoseconds>(end4-start4).count();
+    std::cout<<"conversion time per packet: "<<duration4/rpv.size()<<" \ttotaltime: "<< duration4<<std::endl;
+    if(skippedCount !=0)
+        std::cout<<"tuples size: "<<tuples.size()<<", skipped packets: "<<skippedCount<<std::endl;
+    std::cout<<std::endl;
 
+
+
+    /// serialization with boost
     std::string filename = "testfiles/tuples.dump";
-    int objectcount = tuples.size();
-
+    int objectCount = tuples.size();
+    auto start5 = std::chrono::high_resolution_clock::now();
     {
         std::ofstream ofs(filename);
-        boost::archive::text_oarchive oa(ofs);
+        boost::archive::binary_oarchive oa(ofs);
         for(IPTuple t : tuples)
             oa << t;
     }
+    auto end5 = std::chrono::high_resolution_clock::now();
+    auto duration5 = std::chrono::duration_cast<std::chrono::nanoseconds>(end5-start5).count();
+    std::cout << "writing time per packet: " << duration5 / objectCount << " \ttotaltime: "<< duration5<<std::endl<< std::endl;
+
 
     std::vector<IPTuple> readValues {};
+    auto start6 = std::chrono::high_resolution_clock::now();
     {
         std::ifstream ifs(filename);
-        boost::archive::text_iarchive ia(ifs);
+        boost::archive::binary_iarchive ia(ifs);
         IPTuple temp;
-        for(int i = 0; i < objectcount; ++i) {
+        for(int i = 0; i < objectCount; ++i) {
             ia >> temp;
             readValues.emplace_back(temp);
         }
     }
+    auto end6 = std::chrono::high_resolution_clock::now();
+    auto duration6 = std::chrono::duration_cast<std::chrono::nanoseconds>(end6-start6).count();
+    std::cout << "reading time per packet: " << duration6 / objectCount << " \ttotaltime: "<< duration6<<std::endl<< std::endl;
 
 
-    if(std::equal(tuples.begin(), tuples.end(), readValues.begin())){
-        std::cout<<"vectors are equal \n";
-    }
-    else{
+    bool vectorsAreEqual;
+    auto start7 = std::chrono::high_resolution_clock::now();
+    vectorsAreEqual = std::equal(tuples.begin(), tuples.end(), readValues.begin());
+    auto end7 = std::chrono::high_resolution_clock::now();
+    auto duration7 = std::chrono::duration_cast<std::chrono::nanoseconds>(end7-start7).count();
+    std::cout << "comparison time per packet: " << duration7 / objectCount <<" \ttotaltime: "<< duration7<<std::endl<< std::endl;
+
+    if(!vectorsAreEqual){
         std::cout<<"vectors are not equal\n";
+        return 1;
     }
-
-
-/*    IPTuple writeObject = tuples.at(0);
-    IPTuple writeObject2 = tuples.at(1);
-    std::cout<<"before: "<<writeObject.toString()<<std::endl;
-    std::cout<<"before: "<<writeObject2.toString()<<std::endl;
-
-    {
-        std::ofstream ofs(filename);
-        boost::archive::text_oarchive oa(ofs);
-        // write class instance to archive
-        oa << writeObject << writeObject2;
-        // archive and stream closed when destructors are called
-    }
-
-    IPTuple readObject;
-    IPTuple readObject2;
-    {
-        // create and open an archive for input
-        std::ifstream ifs(filename);
-        boost::archive::text_iarchive ia(ifs);
-        // read class state from archive
-        ia >> readObject >> readObject2;
-        // archive and stream closed when destructors are called
-    }
-
-    std::cout<<"after: "<<readObject.toString()<<std::endl;
-    std::cout<<"after: "<<readObject2.toString()<<std::endl;
-
-*/
-
-
-
-
-
-
-/*    pcpp::Packet p = rpv.at(17);
-
-    std::cout<<"first protocol: "<<std::hex <<p.getFirstLayer()->getProtocol()<<std::endl;
-    printf("%.2x\n", pcpp::DNS);
-    pcpp::DNS;
-    p.removeFirstLayer();
-    std::cout<<"second protocol: "<<std::hex <<p.getFirstLayer()->getProtocol()<<std::endl;
-    p.removeFirstLayer();
-
-
-
-
-    if(p.isPacketOfType(pcpp::IPv4)){
-        pcpp::IPv4Address srcIP = p.getLayerOfType<pcpp::IPv4Layer>()->getSrcIpAddress();
-        pcpp::IPv4Address destIP = p.getLayerOfType<pcpp::IPv4Layer>()->getDstIpAddress();
-        pcpp::ProtocolType protocolType = p.getLayerOfType<pcpp::IPv4Layer>()->getProtocol(); /// ipv4 protocol
-        //if (protocolType == pcpp::ProtocolType.)
-        if(p.isPacketOfType(pcpp::TCP)){
-            std::cout<<"TCP"<<std::endl;
-            protocolType= p.getLayerOfType<pcpp::TcpLayer>()->getProtocol();
-        }
-
-        // print source and dest IPs
-        printf("Source IP is '%s'; Dest IP is '%s'; Protocol is %" PRIu64 "\n",
-               srcIP.toString().c_str(),
-               destIP.toString().c_str(),
-               protocolType);
-
-    }
-*/
-
-
     return 0;
 }
