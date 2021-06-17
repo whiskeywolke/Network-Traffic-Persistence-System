e4d1f56c5258f88645af8c44e7874685749c491b
Michael Mente <a01634435@unet.univie.ac.at>
2021-06-17 09:36:40 +0200

    add runtime query analysis comparison

--- evaluation/reader/query_complexity_plotter.py
+++ evaluation/reader/query_complexity_plotter.py
@@ -1,4 +1,5 @@
 import sys
+import os
 from matplotlib.transforms import Bbox
 import numpy as np
 import matplotlib.pyplot as plt
@@ -8,34 +9,45 @@ from numpy.lib.function_base import median
 def main():
     files = [
     'no_query/reader_evaluation.csv',
-    './simple_query/reader_evaluation_simple.csv',
-    './complex_query/reader_evaluation_complex.csv',
-    './very_complex_query/reader_evaluation_vcomplex.csv']
+    'simple_query/reader_evaluation_simple.csv',
+    'complex_query/reader_evaluation_complex.csv',
+    'very_complex_query/reader_evaluation_vcomplex.csv']
     names = ['no query', 'simple', 'complex','very complex']
 
-#    dataset = np.loadtxt(files[0], delimiter=';', skiprows=1, usecols=0)
+    for folder in [ f.name for f in os.scandir('.') if f.is_dir() ]:
+        datadictHandling = {}
+        datadictWriting = {}
+        datadictTotal = {}
+        for i in range(len(files)):
+            filename = folder + '/' +files[i]
+            dataset = np.loadtxt(filename, delimiter=';', skiprows=1)
+            labels = open(filename).readlines()[0].rstrip().split(';')
+
+            datadictHandling[names[i]] = dataset[:,0]
+            datadictWriting[names[i]] = dataset[:,1]
+            datadictTotal[names[i]] = dataset[:,2]
+
+        np.set_printoptions(precision=4)
+
+        fig1, ax1 = plt.subplots()
+        ax1.set_title("Query Handling Runtime Analysis of " + folder + '\n')
+        ax1.boxplot(datadictHandling.values(), showfliers=True)
+        ax1.set_xticklabels(datadictHandling.keys())
+        plt.savefig("Query Handling Runtime Analysis of " + folder + ".png")
 
 
-    datadict = {}
-    for i in range(len(files)):
-        dataset = np.loadtxt(files[i], delimiter=';', skiprows=1, usecols=0)
-        datadict[names[i]] = dataset
-
-    np.set_printoptions(precision=4)
-
-    fig1, ax1 = plt.subplots()
-    ax1.set_title("Query Runtime Analysis")
-    ax1.boxplot(datadict.values())
-    ax1.set_xticklabels(datadict.keys())
-
-    np.set_printoptions(precision=4)
-    #text = "min = " + str(min(dataset[:,i])) + "\nmax = " + str(max(dataset[:,i])) + "\nmean = " + str(np.round(np.average(dataset[:,i]), 4)) + "\nmedian = " + str(np.round(median(dataset[:,i]),4))
-    #box = dict(boxstyle='round', facecolor='white', alpha=0.5)
-    #ax1.text(0.03,0.8,text, transform=ax1.transAxes, bbox=box)
-    plt.savefig("Query Runtime Analysis.png")
+        fig2, ax2 = plt.subplots()
+        ax2.set_title("Query Writing Runtime Analysis of " + folder + '\n')
+        ax2.boxplot(datadictWriting.values(), showfliers=True)
+        ax2.set_xticklabels(datadictWriting.keys())
+        plt.savefig("Query Writing Runtime Analysis of " + folder + ".png")
 
 
-
+        fig3, ax3 = plt.subplots()
+        ax3.set_title("Query Total Runtime Analysis of " + folder + '\n')
+        ax3.boxplot(datadictTotal.values(), showfliers=True)
+        ax3.set_xticklabels(datadictTotal.keys())
+        plt.savefig("Query Total Runtime Analysis of " + folder + ".png")
 
 
 
