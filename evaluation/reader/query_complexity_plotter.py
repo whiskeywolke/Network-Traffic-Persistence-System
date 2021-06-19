bfc48b39428f5ee1657dd21efb6f908dbe0e3476
Michael M <01634435@unet.univie.ac.at>
2021-06-19 14:28:50 +0200

    add per packet query analysis

--- evaluation/reader/query_complexity_plotter.py
+++ evaluation/reader/query_complexity_plotter.py
@@ -52,6 +52,31 @@ def main():
         ax3.set_ylabel('nanoseconds')
         plt.savefig("Query total runtime analysis of " + folder + ".png")
 
+        if(folder == 'equinix-nyc.dirB.20180517-134900'):
+            print('folder!')
+            fig4, ax4 = plt.subplots()
+            ax4.set_title("Query handling runtime analysis of " + folder + '\n')
+            ax4.boxplot([el/107555567 for el in datadictHandling.values()], showfliers=True)
+            ax4.set_xticklabels(datadictHandling.keys())
+            ax4.set_ylabel('nanoseconds')
+            plt.savefig("Query handling runtime analysis of " + folder + "_pp.png")
+
+
+            fig5, ax5 = plt.subplots()
+            ax5.set_title("Query writing runtime analysis of " + folder + '\n')
+            ax5.boxplot([el/107555567 for el in datadictWriting.values()], showfliers=True)
+            ax5.set_xticklabels(datadictWriting.keys())
+            ax5.set_ylabel('nanoseconds')
+            plt.savefig("Query writing runtime analysis of " + folder + "_pp.png")
+
+
+            fig6, ax6 = plt.subplots()
+            ax6.set_title("Query total runtime analysis of " + folder + '\n')
+            ax6.boxplot([el/107555567 for el in datadictTotal.values()], showfliers=True)
+            ax6.set_xticklabels(datadictTotal.keys())
+            ax6.set_ylabel('nanoseconds')
+            plt.savefig("Query total runtime analysis of " + folder + "_pp.png")
+
 
 
 main()
